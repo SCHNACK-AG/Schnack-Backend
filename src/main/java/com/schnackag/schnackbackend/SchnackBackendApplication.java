044f7c6fb50f5b7d94814b96ede92c204b78360f
Henning Entz <entz.henning@gmail.com>
2024-04-09 23:25:36 +0200

    Initial Commit

---
+++ src/main/java/com/schnackag/schnackbackend/SchnackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.schnackag.schnackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchnackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchnackBackendApplication.class, args);
+	}
+
+}
