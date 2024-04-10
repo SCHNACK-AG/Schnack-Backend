c2e734233dcde9db8f7b3b9bfe19101b2fdf7d1a
Henning Entz <entz.henning@gmail.com>
2024-04-10 11:10:51 +0200

    Added AuthenticationController

---
+++ src/main/java/com/schnackag/schnackbackend/controllers/AuthenticationController.java
@@ -0,0 +1,33 @@
+package com.schnackag.schnackbackend.controllers;
+
+import com.schnackag.schnackbackend.requests.AuthenticationRequest;
+import com.schnackag.schnackbackend.requests.RegisterRequest;
+import com.schnackag.schnackbackend.responses.AuthenticationResponse;
+import com.schnackag.schnackbackend.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/authentication")
+@RequiredArgsConstructor
+public class AuthenticationController {
+	private final AuthenticationService authenticationService;
+
+	@PostMapping("/register")
+	public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+		return ResponseEntity.ok(authenticationService.register(request));
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+		return ResponseEntity.ok(authenticationService.authenticate(request));
+	}
+
+	@Secured({"USER", "MODERATOR", "ADMINISTRATOR"})
+	@GetMapping("/authenticated")
+	public ResponseEntity<Boolean> isAuthenticated() {
+		return ResponseEntity.ok(true);
+	}
+}
