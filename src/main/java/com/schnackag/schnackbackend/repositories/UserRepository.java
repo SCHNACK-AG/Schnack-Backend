cf90245a3b76ee12531f79fdb08d5258d0df40d0
Henning Entz <entz.henning@gmail.com>
2024-04-13 23:38:39 +0200

    Added comments

--- src/main/java/com/schnackag/schnackbackend/repositories/UserRepository.java
+++ src/main/java/com/schnackag/schnackbackend/repositories/UserRepository.java
@@ -6,6 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
+/* Das Repository fungiert als Mittler zwischen der Datenzugriffsschicht und der Anwendungslogikschicht.
+*  Es kapselt die Logik, die Notwendig ist um Daten zu speichern und zu lesen.
+*  Dieses Repository erweitert JpaRepository und stellt somit schon die grundlegenden Operationen zum
+*  Erstellen, Lesen, Ändern und Löschen der Daten bereit. */
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+	/* Spring generiert zur Laufzeit automatisch die Implementierung für die Methode findByEmail(String email).
+	*  https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html#jpa.query-methods.query-creation */
 	Optional<User> findByEmail(String email);
 }
