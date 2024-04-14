b56530b3ac50b384dd6ba30e1f29739c28698038
Henning Entz <entz.henning@gmail.com>
2024-04-14 18:50:13 +0200

    Added comments to JwtService

--- src/main/java/com/schnackag/schnackbackend/services/JwtService.java
+++ src/main/java/com/schnackag/schnackbackend/services/JwtService.java
@@ -17,6 +17,11 @@ import java.util.function.Function;
 public class JwtService {
 	private static final String SECRET_KEY = "ad7646d529b1c1a8a90850522750a3c8d797f557a6de45c1c3c16ff3418cc8df";
 
+	/* Diese Methode nimmt ein JWT als String und gibt die Claim-Daten in Form
+	 * eines Claims-Objekts zurück. Ein Claim in JWT ist eine Aussage über einen Benutzer,
+	 * die Daten über den Benutzer und zusätzliche Metadaten enthält.
+	 * Diese Methode verwendet das Keys.hmacShaKeyFor()-Verfahren und den Schlüssel,
+	 * um das Token zu entschlüsseln und die Ansprüche zu extrahieren.*/
 	private Claims extractAllClaims(String token) {
 		return Jwts
 				.parser()
@@ -26,19 +31,26 @@ public class JwtService {
 				.getPayload();
 	}
 
+	/* Diese Methode dient zum Extrahieren eines spezifischen Claims aus dem Token.
+	 * Sie nimmt eine Funktion als Parameter, die angibt, welcher Claim extrahiert werden soll. */
 	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
 		final Claims claims = extractAllClaims(token);
 		return claimsResolver.apply(claims);
 	}
 
+	/* Diese Methode verwendet die extractClaim()-Methode zur Extrahierung des Ablaufdatums des Tokens. */
 	private Date extractExpiration(String token) {
 		return extractClaim(token, Claims::getExpiration);
 	}
 
+	/* Diese Methode, verwendet die extractClaim()-Methode zur Extrahierung des Benutzernamens aus dem Token. */
 	public String extractUsername(String token) {
 		return extractClaim(token, Claims::getSubject);
 	}
 
+	/* generateToken(UserDetails userDetails) und generateToken(Map<String, Object> extraClaims, UserDetails userDetails):
+	 * Diese Methoden werden verwendet, um ein neues JWT zu erstellen. Sie nehmen ein UserDetails-Objekt und ggf.
+	 * eine Map von zusätzlichen Claims als Parameter. Das Token wird verschlüsselt und als String zurückgegeben. */
 	public String generateToken(UserDetails userDetails) {
 		return generateToken(new HashMap<>(), userDetails);
 	}
@@ -54,15 +66,22 @@ public class JwtService {
 				.compact();
 	}
 
+	/* Diese Methode konvertiert den gegebenen Schlüssel (als Base64-String) in ein SecretKey-Objekt,
+	 * das zum Verschlüsseln und Entschlüsseln des JWT verwendet wird. */
 	private SecretKey getSignInKey() {
 		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
 		return Keys.hmacShaKeyFor(keyBytes);
 	}
 
+	/* Diese Methode prüft, ob das Token abgelaufen ist,
+	 * indem sie das Ablaufdatum des Tokens mit dem aktuellen Datum vergleicht. */
 	private boolean isTokenExpired(String token) {
 		return extractExpiration(token).before(new Date());
 	}
 
+	/* Diese Methode verwendet extractUsername() und isTokenExpired() zur Überprüfung der Gültigkeit des Tokens.
+	 * Sie gibt true zurück, wenn der Benutzername des Tokens dem Benutzernamen
+	 * des übergebenen UserDetails-Objekts entspricht und das Token nicht abgelaufen ist. */
 	public boolean isTokenValid(String token, UserDetails userDetails) {
 		final String username = extractUsername(token);
 		return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
