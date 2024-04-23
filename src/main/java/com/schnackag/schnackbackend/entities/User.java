bc1309c9896f8467416bcec5fcd206feb156cd5c
Henning Entz <entz.henning@gmail.com>
2024-04-23 15:40:38 +0200

    Added Entity Relationships

--- src/main/java/com/schnackag/schnackbackend/entities/User.java
+++ src/main/java/com/schnackag/schnackbackend/entities/User.java
@@ -19,41 +19,47 @@ import java.util.UUID;
 @AllArgsConstructor
 @Entity
 public class User implements UserDetails {
-	@Id
-	@GeneratedValue(strategy = GenerationType.UUID)
-	private UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-	private String email;
+    private String email;
 
-	private String password;
+    private String password;
 
-	private String username;
+    private String username;
 
-	@Enumerated(EnumType.STRING)
-	private Role role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of(new SimpleGrantedAuthority(role.name()));
-	}
+    @OneToMany(mappedBy = "owner")
+    private List<Thread> threads;
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    @OneToMany(mappedBy = "author")
+    private List<Post> posts;
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
